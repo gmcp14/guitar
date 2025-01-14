321797b53477e347a9ad1ba874f803fc8dbe03f3
Glennylych <melanic2014@gmail.com>
2025-01-14 08:54:25 -0400

    modificado

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,22 @@
-import {useState} from 'react'
+import {useState,useEffect} from 'react'
 import Header from "./components/Header"
 import Guitar from "./components/Guitar"
 import { db } from './data/db'
 
 function App() {
+
+  const initialCart = () => {
+        const localStorageCart= localStorage.getItem('cart')
+        return localStorageCart ? JSON.parse(localStorageCart) : []
+   }
    const [data, setData] = useState(db)
-   const [cart, setCart] = useState([])  
+   const [cart, setCart] = useState(initialCart)  
    const MIN_ITEMS = 1
    const MAX_ITEMS = 5
+
+   useEffect( () => {
+        localStorage.setItem('cart',JSON.stringify(cart))
+   },[cart])
    
    function addToCart(item){
         const itemExists= cart.findIndex(guitar => guitar.id === item.id )
@@ -22,6 +31,7 @@ function App() {
             
             setCart([...cart, item])
         }
+        
     }
 
     function removeFRomCart(id){
@@ -58,6 +68,7 @@ function App() {
     function clearCart(e){
         setCart([])
     }
+    
    
    
     return (
