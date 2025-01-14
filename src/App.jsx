c8737f13990e467da752d87a9583837c6a695121
Glennylych <melanic2014@gmail.com>
2025-01-14 09:07:44 -0400

    ultima modificacion

--- src/App.jsx
+++ src/App.jsx
@@ -9,7 +9,7 @@ function App() {
         const localStorageCart= localStorage.getItem('cart')
         return localStorageCart ? JSON.parse(localStorageCart) : []
    }
-   const [data, setData] = useState(db)
+   const [data] = useState(db)
    const [cart, setCart] = useState(initialCart)  
    const MIN_ITEMS = 1
    const MAX_ITEMS = 5
